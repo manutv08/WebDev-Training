f9ba5bbf33f336c944688edb55fd23fc1cc53a9c
Manu Varghese T <manutv08@gmail.com>
2022-05-09 12:29:17 +0530

    6th may HW correction

--- js/6th_May_HW.js
+++ js/6th_May_HW.js
@@ -17,14 +17,16 @@ function getToDo() {
     btn.id = `btn_${arr.length}`
     btn.setAttribute("onclick", `removeTask(${arr.length})`)
     l.appendChild(btn)
+    console.log(arr);
 }
 
 function removeTask(index) {
     arr.splice(index-1,1)
     document.getElementById(index).remove()
     for(let i=index+1; i<=arr.length+1; i++){
-        document.getElementById(i).setAttribute("id", `${i-1}`)
         document.getElementById(`btn_${i}`).setAttribute("onclick", `removeTask(${i-1})`)
+        document.getElementById(`btn_${i}`).setAttribute("id", `btn_${i-1}`)
+        document.getElementById(i).setAttribute("id", `${i-1}`)
         console.log(document.getElementById("mylist"));
     }
     console.log(arr);
