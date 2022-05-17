0769538a0eccc66d0f3808ecb17ab942f12be62a
Manu Varghese T <manutv08@gmail.com>
2022-05-17 08:23:29 +0530

    form hw completed

--- js/validation.js
+++ js/validation.js
@@ -9,12 +9,24 @@ function validate(){
         name.focus()
         return false
     }
-    let regex = /^[A-Za-z]+[\w\W]*@(gmail.com|yahoo.in)$/
-    if(email.value==="" && regex.test()){
+    let regex_e = /^[A-Za-z]+[\w\W]*@[a-zA-Z0-9]+\.[a-z]{2,3}$/
+    if(!regex_e.test(email.value)){
+        alert("Invalid email")
+        email.focus()
+        return false
+    }
+    if(email.value==="" ){
         alert("email field can't be empty")
         email.focus()
         return false
     }
+    let regex_p = /^[\w\W]{5,8}$/
+    if(!regex_p.test(password.value)){
+        alert("Password length must be between 5 to 8")
+        password.focus()
+        return false
+    }
+    
 
     if(password.value===""){
         alert("Password field can't be empty")
@@ -34,16 +46,16 @@ function validate(){
     }
 }
 
-let myarry=[10,20,30,40,50]
-let myarry2=[10,20,30,40,90]
-myarry=[90,...myarry]
-console.log(myarry);
-let obj1={x1:10,y1:20}
-let obj={...obj1,y1:30,z:100}
-console.log(obj);
-let myarr=[...myarry,...myarry2]
-console.log(myarr);
-document.write("\u00A3")
-document.write("\u4F60")
-document.write("\u597D")
-document.write("😑")
+// let myarry=[10,20,30,40,50]
+// let myarry2=[10,20,30,40,90]
+// myarry=[90,...myarry]
+// console.log(myarry);
+// let obj1={x1:10,y1:20}
+// let obj={...obj1,y1:30,z:100}
+// console.log(obj);
+// let myarr=[...myarry,...myarry2]
+// console.log(myarr);
+// document.write("\u00A3")
+// document.write("\u4F60")
+// document.write("\u597D")
+// document.write("😑")
